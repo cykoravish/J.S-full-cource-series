8028c049c8e7fca9989fadaa448e648d1ce0dde1
Ravish Bisht <ravishbisht85@gmail.com>
2023-10-22 07:11:17 +0000

    01_variables file added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553;
+let accountEmail = "cykoravish@gmail.com";
+var accountPassword = "12345";
+accountCity = "uttarakhand";
+let accountState;
+// accountId = 10;  //not allowed
+accountEmail = "ravish@gmail.com";
+accountPassword = "54321";
+accountCity = 'USA';
+
+// console.log(accountId)
+
+/*prefer not to use var because of issue in block scope and functional scope */
+
+console.log([accountId, accountEmail,accountPassword,accountCity, accountState])
