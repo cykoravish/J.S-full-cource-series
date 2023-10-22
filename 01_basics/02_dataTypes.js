30fc3d961710468c68b9ad7959c364387b433c22
Ravish Bisht <ravishbisht85@gmail.com>
2023-10-22 10:45:38 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" //treat all js course as newer version
+
+// alert(3+3); //we are using nodejs, not browser so it won't work
+
+// console.log(3
+//     +3) //code readability should be high
+
+// console.log("ravish");
+
+// let name = "ravish"
+// let age = 18
+// let isLoggedIn = false
+// let state;
+
+         // primitive dataTypes in javascript
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standlone value
+//undefined => value not assigned
+//symbol => unique
+
+//object
+console.log(typeof "ravish") //undefined
+console.log(typeof null)  //object
