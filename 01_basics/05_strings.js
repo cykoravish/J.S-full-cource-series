839673d69fa2bfb4aa77fc29f59a5520003b4fe1
Ravish Bisht <ravishbisht85@gmail.com>
2023-10-23 12:36:21 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,33 @@
+// const name = "ravish";
+// const repoCount = 40;
+
+// console.log(name + repoCount + " value");
+
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String("Ravish");
+// console.log(typeof gameName)
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(1))
+// console.log(gameName.indexOf("a"))
+
+// const newString = gameName.substring(0,4);
+// console.log(newString)
+
+// const anotherString = gameName.slice(0,4)
+// console.log(anotherString)
+
+// const newStringOne = "   ravish   "
+// console.log(newStringOne) 
+// console.log(newStringOne.trim()) 
+
+const url = "https://hitesh.com/hitesh%20choudhary";
+// console.log(url.replace("%20", "-"))
+// console.log(url.includes("hitesh"))
+// console.log(url.includes("ravi"))
+
+console.log(url.split("/"))
