24406a490ca04f1d9ee35ae90d81ed60d049c8de
Ravish Bisht <ravishbisht85@gmail.com>
2023-10-23 04:01:28 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// we can use Number() Boolean() String() to change the data type of variables;
+
+// let score = "12abc";
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+// let isLoggedIn = "ravish";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+//"hitesh" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber)
