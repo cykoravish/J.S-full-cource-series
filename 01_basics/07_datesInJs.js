532f97b10155a93240127ab6073d8a113fdca97e
Ravish Bisht <ravishbisht85@gmail.com>
2023-10-23 13:35:02 +0000

    dates

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,20 @@
+//Dates
+
+// let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// console.log(myCreatedDate.toDateString())
+
+// let myTimeStamp = Date.now();
+
+let newDate = new Date();
+// console.log(newDate)
+// console.log(newDate.getMonth() + 1)
+// console.log(newDate.getDay())
+
+console.log(newDate.toLocaleString("default", {
+    weekday: "long",
+}))
