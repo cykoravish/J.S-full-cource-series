4d193481f5445cba273ff509be22d189450f0d77
Ravish Bisht <ravishbisht85@gmail.com>
2023-10-23 06:28:08 +0000

    dataTypes summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,41 @@
+// Primitive DataTypes
+
+// 7 types : Null, Number, Symbol, String, Boolean, BigInt, undefined
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null;
+// console.log(typeof outsideTemp)
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId)
+
+// const bigNumber = 25656545455545454454544n
+// console.log(typeof bigNumber)
+
+//Non-Primitive or Reference
+
+//Array, Objects, Functions
+
+// const heros = ["shaktimaan", "naagraj", "spiderman"]
+// let myObj = {
+//     name: "ravish",
+//     age:24,
+// }
+
+// const myFunction = function(){
+//     console.log("hello Ravish")
+// }
+// console.log(typeof myFunction) //function-object
+
+// type of all datatypes :- refer to documentation
+// undefined - undefined 
+// Null - object
+// Bollean - Boolean
+// Number - Number
+// String - String
+// object - object
